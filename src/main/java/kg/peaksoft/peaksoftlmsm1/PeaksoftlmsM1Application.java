0c71e57d53be36d416fc0b3bf05f17926ebb9bc7
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-05-17 18:18:40 +0600

    empty project

---
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/PeaksoftlmsM1Application.java
@@ -0,0 +1,13 @@
+package kg.peaksoft.peaksoftlmsm1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeaksoftlmsM1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeaksoftlmsM1Application.class, args);
+	}
+
+}
---
+++ src/test/java/kg/peaksoft/peaksoftlmsm1/PeaksoftlmsM1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.peaksoft.peaksoftlmsm1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeaksoftlmsM1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
