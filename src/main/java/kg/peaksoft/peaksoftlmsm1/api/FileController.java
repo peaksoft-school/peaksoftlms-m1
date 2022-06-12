98ab4d17c14213cc104b459ba8b8b3661d1bde57
bekmuratsatyboldiev98gmail.com <bekasatyboldiev>
2022-06-12 10:54:34 +0300

    refacto

--- src/main/java/kg/peaksoft/peaksoftlmsm1/api/FileController.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/api/FileController.java
@@ -3,53 +3,66 @@ package kg.peaksoft.peaksoftlmsm1.api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.peaksoft.peaksoftlmsm1.db.entity.models.FilePath;
-import kg.peaksoft.peaksoftlmsm1.db.service.FilePathService;
+import kg.peaksoft.peaksoftlmsm1.db.service.S3Service;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.util.List;
+import static java.net.HttpURLConnection.HTTP_OK;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/files")
 @Tag(name = "File controller", description = "Instructor can create, update, and delete")
+@RequestMapping("api/files")
 public class FileController {
 
-    private final FilePathService filePathService;
+    private final S3Service s3Service;;
 
-    @ResponseBody
-    @Operation(summary = "method create file", description = "instructor can upload file")
     @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
+    @Operation(summary = "method create", description = "Only Instructor can create file")
     @PostMapping
     public String upload(@RequestParam("file") MultipartFile file){
-        return filePathService.upload(file);
+        return s3Service.saveFile(file);
     }
 
-    @Operation(summary = "method delete file", description = "instructor can delete file")
+    /**
+     *  Метод download загружает файл из сервера и показывает в Postman
+     */
     @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
-    @DeleteMapping("{id}")
-    public ResponseEntity<FilePath> delete(@PathVariable Long id) {
-        filePathService.delete(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @Operation(summary = "method download file", description = "Instructor can download file")
+    @GetMapping("download/{id}")
+    public ResponseEntity<byte[]> download(@PathVariable Long id){
+        HttpHeaders headers=new HttpHeaders();
+        headers.add("Content-type", MediaType.ALL_VALUE);
+        headers.add("Content-Disposition", "attachment; filename="+id);
+        byte[] bytes = s3Service.downloadFile(id);
+        return  ResponseEntity.status(HTTP_OK).headers(headers).body(bytes);
     }
 
-    @Operation(summary = "method get file by id ", description = "instructor can found get by id")
     @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
+    @Operation(summary = "method get by id", description = "Instructor can get by id file")
     @GetMapping("{id}")
     public ResponseEntity<FilePath> getById(@PathVariable Long id) {
-        return ResponseEntity.ok(filePathService.getById(id).get());
+        return ResponseEntity.ok(s3Service.getById(id));
     }
 
-    @Operation(summary = "method get all files", description = "Instructor can get all files")
     @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
+    @Operation(summary = "method delete", description = "Only Instructor can delete file")
+    @DeleteMapping("{id}")
+    public  String delete(@PathVariable Long id){
+        return s3Service.deleteFile(id);
+    }
+
+    @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
+    @Operation(summary = "method get all", description = "Only Instructor can get all file")
     @GetMapping
-    public ResponseEntity<List<FilePath>> getAll() {
-        return ResponseEntity.ok(filePathService.getAllFile());
+    public List<String> getAll(){
+        return s3Service.listAllFiles();
     }
 
 }
