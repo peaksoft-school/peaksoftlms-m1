73756a61d2d2e3216a085b1d7c4f93cb1837465b
Zamira <keldibaevaz@gmail.com>
2022-07-10 12:25:42 +0300

    added Option Service Test

--- src/main/java/kg/peaksoft/peaksoftlmsm1/db/dto/test/request/response/OptionResponse.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/db/dto/test/request/response/OptionResponse.java
@@ -2,11 +2,15 @@ package kg.peaksoft.peaksoftlmsm1.db.dto.test.request.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class OptionResponse {
 
