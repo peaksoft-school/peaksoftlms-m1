7423e6905a4adca4f32595c7d5d925a83413ebc5
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-21 12:51:41 +0600

    refactor AdminApi v2

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/AdminApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/AdminApi.java
@@ -28,24 +28,23 @@ import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/admin")
-@Tag(name = "AdminController", description = "ADMIN can create, update, get by Id, delete, get all Teachers and Students")
+@PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
+@Tag(name = "Admin API", description = "ADMIN can create, update, get by Id, delete, get all teachers and students")
 public class AdminApi {
 
     private final TeacherService teacherService;
     private final StudentService studentService;
 
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method create", description = "admin can create Teacher")
     @PostMapping("/teachers")
     public ResponseEntity<TeacherResponse> create(@RequestBody @Valid TeacherRequest request) {
         return new ResponseEntity<>(teacherService.create(request), HttpStatus.CREATED);
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method update", description = "admin can update Teacher")
     @PutMapping("/teachers/{id}")
     public ResponseEntity<TeacherResponse> update(@PathVariable Long id, @Valid @RequestBody TeacherRequest request) {
@@ -53,14 +52,12 @@ public class AdminApi {
         return new ResponseEntity<>(teacherResponse, HttpStatus.OK);
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method getByID", description = "admin can getById Teacher")
     @GetMapping("/teachers/{id}")
     public ResponseEntity<TeacherResponse> getById(@PathVariable Long id) {
         return ResponseEntity.ok(teacherService.getById(id));
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method delete", description = "admin can delete Teacher")
     @DeleteMapping("/teachers/{id}")
     public ResponseEntity<TeacherResponse> delete(@PathVariable Long id) {
@@ -68,21 +65,18 @@ public class AdminApi {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method get all Teachers", description = "admin can get all Teachers")
     @GetMapping("/teachers/all")
     public List<TeacherResponse> getAllTeachers() {
         return teacherService.getAll();
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method create", description = "admin can create Student")
     @PostMapping("/students")
     public ResponseEntity<StudentResponse> create(@RequestBody @Valid StudentRequest request) {
         return new ResponseEntity<>(studentService.create(request), HttpStatus.CREATED);
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method update", description = "admin can update Student")
     @PutMapping("/students/{id}")
     public ResponseEntity<StudentResponse> update(@PathVariable Long id, @Valid @RequestBody StudentRequest request) {
@@ -90,14 +84,12 @@ public class AdminApi {
         return new ResponseEntity<>(studentResponse, HttpStatus.OK);
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method get by id", description = "admin, instructor can get by id")
     @GetMapping("/students/{id}")
     public ResponseEntity<StudentResponse> getByIdStudent(@PathVariable Long id) {
         return ResponseEntity.ok(studentService.getById(id));
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method delete", description = "admin can delete Student")
     @DeleteMapping("/students/{id}")
     public ResponseEntity<StudentResponse> deleteStudent(@PathVariable Long id) {
@@ -105,14 +97,12 @@ public class AdminApi {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method get all Students", description = "admin can get all Students")
     @GetMapping("/students/all")
     public List<StudentResponse> getAllStudents() {
         return studentService.getAll();
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "Excel File import",
             description = "Admin can import excel File")
     @PostMapping("/students/importExcel")
