0f4856df9f4b4d2f640ece8816f7eef028aeb083
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 16:05:31 +0600

    refactor OptionController

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/OptionController.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/OptionController.java
@@ -6,15 +6,21 @@ import kg.peaksoft.peaksoftlmsm1.db.service.testService.OptionService;
 import kg.peaksoft.peaksoftlmsm1.controller.dto.test.request.OptionRequest;
 import kg.peaksoft.peaksoftlmsm1.controller.dto.test.request.response.OptionResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
@@ -29,7 +35,6 @@ public class OptionController {
     @PostMapping("/{questionId}")
     public ResponseEntity<OptionResponse> save(@PathVariable Long questionId,
                                                @RequestBody @Valid OptionRequest optionRequest) {
-        log.info("inside OptionController create method");
         return new ResponseEntity<>(optionService.save(questionId, optionRequest), HttpStatus.CREATED);
     }
 
@@ -37,8 +42,7 @@ public class OptionController {
     @Operation(summary = "method update", description = "Instructor can update Option")
     @PutMapping("{id}")
     public ResponseEntity<OptionResponse> update(@PathVariable Long id,
-                                                 @RequestBody @Valid OptionRequest request){
-        log.info("inside OptionController update method");
+                                                 @RequestBody @Valid OptionRequest request) {
         OptionResponse optionResponse = optionService.update(id, request);
         return new ResponseEntity<>(optionResponse, HttpStatus.OK);
     }
@@ -47,7 +51,6 @@ public class OptionController {
     @Operation(summary = "method get by id", description = "Instructor can get by id Option")
     @GetMapping("{id}")
     public ResponseEntity<OptionResponse> getById(@PathVariable Long id) {
-        log.info("inside OptionController get by id method");
         return ResponseEntity.ok(optionService.getById(id));
     }
 
@@ -55,7 +58,6 @@ public class OptionController {
     @Operation(summary = "method delete", description = "Instructor can delete Option")
     @DeleteMapping("{id}")
     public ResponseEntity<OptionResponse> delete(@PathVariable Long id) {
-        log.info("inside OptionController delete method");
         optionService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
