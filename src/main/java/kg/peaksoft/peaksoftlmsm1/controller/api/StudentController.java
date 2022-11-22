0a30b651ce788b79462c447c4f1935405ae39169
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 16:19:50 +0600

    refactor StudentController

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/StudentController.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/StudentController.java
@@ -9,17 +9,20 @@ import kg.peaksoft.peaksoftlmsm1.db.entity.Course;
 import kg.peaksoft.peaksoftlmsm1.db.service.CourseService;
 import kg.peaksoft.peaksoftlmsm1.db.service.testService.ResultService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Slf4j
-@CrossOrigin(origins = "*", allowedHeaders = "*",maxAge = 3600)
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "Student controller", description = "STUDENT can use this controller")
@@ -49,8 +52,7 @@ public class StudentController {
     @Operation(summary = "method get all rating Students", description = "Student can get all rating")
     @GetMapping("rating")
     public RatingListResponse getAllRatingByTests(@RequestParam int size,
-                                                  @RequestParam int page){
-        log.info("inside CourseController get all method");
+                                                  @RequestParam int page) {
         return resultService.getStudentsRating(size, page);
     }
 }
