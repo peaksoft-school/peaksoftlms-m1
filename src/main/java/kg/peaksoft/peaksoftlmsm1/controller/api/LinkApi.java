40d8d6d2e308ba1b6aa5d6b4fda077f45e001626
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 13:44:51 +0600

    refactor LinkApi v2

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/LinkApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/LinkApi.java
@@ -21,23 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "link controller", description = "INSTRUCTOR can create, update and delete")
-@RequestMapping("api/teachers/links")
+@RequestMapping("api/links")
+@PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
+@Tag(name = "Link API", description = "Link endpoints for instructor")
 public class LinkApi {
 
     private final LinkService linkService;
 
-    @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
     @Operation(summary = "method create", description = "Only Instructor can create link")
     @PostMapping
     public ResponseEntity<LinkResponse> create(@RequestBody @Valid LinkRequest request) {
         return new ResponseEntity<>(linkService.create(request), HttpStatus.CREATED);
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
     @Operation(summary = "method update", description = "Only Instructor can update link")
     @PutMapping("{id}")
     public ResponseEntity<LinkResponse> update(@PathVariable Long id, @Valid @RequestBody LinkRequest request) {
@@ -45,18 +44,17 @@ public class LinkApi {
         return new ResponseEntity<>(linkResponse, HttpStatus.OK);
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
     @Operation(summary = "method get by id", description = "Instructor can get by id link")
     @GetMapping("{id}")
     public ResponseEntity<LinkResponse> getById(@PathVariable Long id) {
         return ResponseEntity.ok(linkService.getById(id));
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
     @Operation(summary = "method delete", description = "Only Instructor can delete link")
     @DeleteMapping("{id}")
     public ResponseEntity<LinkResponse> delete(@PathVariable Long id) {
         linkService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
 }
