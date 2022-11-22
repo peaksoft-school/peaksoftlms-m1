a8c11a0313660ce84e831319002efb61f5acd4c8
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 17:26:05 +0600

    refactor TestResponse

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/dto/test/response/TestResponse.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/dto/test/response/TestResponse.java
@@ -1,7 +1,5 @@
 package kg.peaksoft.peaksoftlmsm1.controller.dto.test.response;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import kg.peaksoft.peaksoftlmsm1.db.entity.testEntity.Question;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,11 +12,10 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class TestResponse {
 
-     private Long id;
-     private String name;
-     private List<Question> questionList;
+    private Long id;
+    private String name;
+    private List<Question> questionList;
 
 }
