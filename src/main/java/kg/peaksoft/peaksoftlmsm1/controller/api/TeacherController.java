14a68050310e47bb4d5731e7fca62f8e1f7027ad
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 16:26:55 +0600

    refactor TeacherController

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/TeacherController.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/TeacherController.java
@@ -9,19 +9,22 @@ import kg.peaksoft.peaksoftlmsm1.db.entity.User;
 import kg.peaksoft.peaksoftlmsm1.db.entity.Course;
 import kg.peaksoft.peaksoftlmsm1.db.service.CourseService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*", allowedHeaders = "*",maxAge = 3600)
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @Tag(name = "Teacher controller", description = "INSTRUCTOR can get and add")
 @RequestMapping("api/teachers")
 public class TeacherController {
@@ -31,8 +34,7 @@ public class TeacherController {
     @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
     @Operation(summary = "method get Courses by User", description = "instructor can get own Courses")
     @GetMapping("/courses")
-    public List<Course> getCoursesByUser(Authentication authentication){
-        log.info("inside TeacherController getCoursesByUser method");
+    public List<Course> getCoursesByUser(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         return user.getCourses();
     }
@@ -42,9 +44,8 @@ public class TeacherController {
     @PostMapping("/courses/{courseId}/{studentId}")
     public ResponseEntity<CourseResponce> addStudentToCourse(
             @PathVariable("studentId") Long studentId,
-            @PathVariable("courseId") Long courseId){
-        log.info("inside TeacherController addStudentToCourse method");
-        return new ResponseEntity<>(courseService.addStudentToCourse(courseId,studentId), HttpStatus.OK);
+            @PathVariable("courseId") Long courseId) {
+        return new ResponseEntity<>(courseService.addStudentToCourse(courseId, studentId), HttpStatus.OK);
     }
 
     @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
@@ -52,9 +53,8 @@ public class TeacherController {
     @PostMapping("/courses/groups/{courseId}/{groupId}")
     public ResponseEntity<CourseResponce> addGroupToCourse(
             @PathVariable("groupId") Long groupId,
-            @PathVariable("courseId")  Long courseId){
-        log.info("inside TeacherController addGroupToCourse method");
-        return new ResponseEntity<>(courseService.addGroupToCourse(courseId,groupId), HttpStatus.OK);
+            @PathVariable("courseId") Long courseId) {
+        return new ResponseEntity<>(courseService.addGroupToCourse(courseId, groupId), HttpStatus.OK);
     }
 
     @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
