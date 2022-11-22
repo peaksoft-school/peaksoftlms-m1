d6caff2e2a1e169256852be9831b57311eaf18fd
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 13:42:26 +0600

    refactor LinkController

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/LinkController.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/LinkController.java
@@ -6,15 +6,21 @@ import kg.peaksoft.peaksoftlmsm1.controller.dto.link.LinkRequest;
 import kg.peaksoft.peaksoftlmsm1.controller.dto.link.LinkResponse;
 import kg.peaksoft.peaksoftlmsm1.db.service.LinkService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Slf4j
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
 @RequiredArgsConstructor
@@ -27,16 +33,14 @@ public class LinkController {
     @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
     @Operation(summary = "method create", description = "Only Instructor can create link")
     @PostMapping
-    public ResponseEntity<LinkResponse> create(@RequestBody @Valid LinkRequest request){
-        log.info("inside LinkController create method");
+    public ResponseEntity<LinkResponse> create(@RequestBody @Valid LinkRequest request) {
         return new ResponseEntity<>(linkService.create(request), HttpStatus.CREATED);
     }
 
     @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
     @Operation(summary = "method update", description = "Only Instructor can update link")
     @PutMapping("{id}")
-    public ResponseEntity<LinkResponse> update(@PathVariable Long id, @Valid @RequestBody LinkRequest request){
-        log.info("inside LinkController update method");
+    public ResponseEntity<LinkResponse> update(@PathVariable Long id, @Valid @RequestBody LinkRequest request) {
         LinkResponse linkResponse = linkService.update(id, request);
         return new ResponseEntity<>(linkResponse, HttpStatus.OK);
     }
@@ -45,7 +49,6 @@ public class LinkController {
     @Operation(summary = "method get by id", description = "Instructor can get by id link")
     @GetMapping("{id}")
     public ResponseEntity<LinkResponse> getById(@PathVariable Long id) {
-        log.info("inside LinkController getById method");
         return ResponseEntity.ok(linkService.getById(id));
     }
 
@@ -53,7 +56,6 @@ public class LinkController {
     @Operation(summary = "method delete", description = "Only Instructor can delete link")
     @DeleteMapping("{id}")
     public ResponseEntity<LinkResponse> delete(@PathVariable Long id) {
-        log.info("inside LinkController delete method");
         linkService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
