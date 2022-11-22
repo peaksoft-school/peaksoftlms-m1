56839f03c2a2235019c72e788b8576c794f20f4c
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 13:19:22 +0600

    refactor CourseController

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/CourseController.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/CourseController.java
@@ -6,21 +6,29 @@ import kg.peaksoft.peaksoftlmsm1.controller.dto.course.CourseRequest;
 import kg.peaksoft.peaksoftlmsm1.controller.dto.course.CourseResponce;
 import kg.peaksoft.peaksoftlmsm1.controller.dto.responseAll.CourseResponseAll;
 import kg.peaksoft.peaksoftlmsm1.db.service.CourseService;
+
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Slf4j
-@CrossOrigin(origins = "*", allowedHeaders = "*",maxAge = 3600)
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "Course controller", description = "ADMIN create, update and delete")
-@RequestMapping("api/admin/courses")
+@RequestMapping("api/courses")
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
+@Tag(name = "Course API", description = "Course endpoints")
 public class CourseController {
 
     private final CourseService courseService;
@@ -28,17 +36,15 @@ public class CourseController {
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method create", description = "admin can registration course")
     @PostMapping
-    public ResponseEntity<CourseResponce> create(@RequestBody @Valid CourseRequest request){
-        log.info("inside CourseController create method");
+    public ResponseEntity<CourseResponce> create(@RequestBody @Valid CourseRequest request) {
         return new ResponseEntity<>(courseService.save(request), HttpStatus.CREATED);
     }
 
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method update", description = "admin can update course")
     @PutMapping("{id}")
-    public ResponseEntity<CourseResponce> update(@PathVariable Long id, @Valid @RequestBody CourseRequest request){
+    public ResponseEntity<CourseResponce> update(@PathVariable Long id, @Valid @RequestBody CourseRequest request) {
         CourseResponce courseResponse = courseService.update(id, request);
-        log.info("inside CourseController update method");
         return new ResponseEntity<>(courseResponse, HttpStatus.OK);
     }
 
@@ -46,7 +52,6 @@ public class CourseController {
     @Operation(summary = "method get by id", description = "admin, instructor can get by id")
     @GetMapping("{id}")
     public ResponseEntity<CourseResponce> getById(@PathVariable Long id) {
-        log.info("inside CourseController get by id method");
         return ResponseEntity.ok(courseService.getById(id));
     }
 
@@ -54,7 +59,6 @@ public class CourseController {
     @Operation(summary = "method delete", description = "admin can delete")
     @DeleteMapping("{id}")
     public ResponseEntity<CourseResponce> delete(@PathVariable Long id) {
-        log.info("inside CourseController delete method");
         courseService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -63,8 +67,7 @@ public class CourseController {
     @Operation(summary = "method get all", description = "admin can get all")
     @GetMapping
     public CourseResponseAll getAll(@RequestParam int size,
-                                    @RequestParam int page){
-        log.info("inside CourseController get all method");
+                                    @RequestParam int page) {
         return courseService.getAllCourses(size, page);
     }
 
