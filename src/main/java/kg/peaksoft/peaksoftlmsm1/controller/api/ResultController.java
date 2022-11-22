cca263459a7e2b5082d6193b71a2c07de3b43ae4
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 16:15:47 +0600

    refactor ResultController

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/ResultController.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/ResultController.java
@@ -7,14 +7,18 @@ import kg.peaksoft.peaksoftlmsm1.db.service.testService.ResultService;
 import kg.peaksoft.peaksoftlmsm1.controller.dto.test.request.AnswerRequest;
 import kg.peaksoft.peaksoftlmsm1.controller.dto.test.request.response.ResultResponse;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Slf4j
 @RestController
 @AllArgsConstructor
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -22,13 +26,12 @@ import java.util.List;
 @RequestMapping("/api/teachers/results")
 public class ResultController {
 
-    private  final ResultService resultService;
+    private final ResultService resultService;
 
     @PreAuthorize("hasAnyAuthority('ROLE_STUDENT')")
     @Operation(summary = "method save Result", description = "Saving Student responses")
     @PostMapping("/myTest")
     public ResultResponse saveResult(Authentication authentication, @RequestBody AnswerRequest answerRequest) {
-        log.info("inside ResultController save method");
         User user = (User) authentication.getPrincipal();
         return resultService.saveResult(answerRequest, user.getId());
     }
@@ -37,7 +40,6 @@ public class ResultController {
     @Operation(summary = "method get Results", description = "Instructor can get Results")
     @GetMapping("/{testId}")
     public List<ResultResponse> getResults(@PathVariable Long testId) {
-        log.info("inside ResultController get all method");
         return resultService.getAllResultByTestId(testId);
     }
 
