0139a3113eb14b802051f1c2a1a841db29ef0d9c
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 17:25:41 +0600

    refactor ResultResponse

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/dto/test/response/ResultResponse.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/dto/test/response/ResultResponse.java
@@ -1,16 +1,17 @@
 package kg.peaksoft.peaksoftlmsm1.controller.dto.test.response;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import kg.peaksoft.peaksoftlmsm1.db.entity.User;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ResultResponse {
 
     private Long id;
