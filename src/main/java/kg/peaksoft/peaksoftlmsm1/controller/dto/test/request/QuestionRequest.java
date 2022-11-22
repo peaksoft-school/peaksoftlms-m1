cb9dcc980cbf4021f90243506e4c820bd191f154
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 17:22:55 +0600

    refactor QuestionRequest

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/dto/test/request/QuestionRequest.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/dto/test/request/QuestionRequest.java
@@ -1,7 +1,5 @@
 package kg.peaksoft.peaksoftlmsm1.controller.dto.test.request;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import kg.peaksoft.peaksoftlmsm1.db.enums.EQuestionType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,7 +10,6 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class QuestionRequest {
 
     private String questionTitle;
