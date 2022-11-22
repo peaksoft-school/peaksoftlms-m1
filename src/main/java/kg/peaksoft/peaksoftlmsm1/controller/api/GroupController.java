6988392caed3c227437b2097a80f2d9884a56b44
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 13:30:45 +0600

    refactor GroupController

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/GroupController.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/GroupController.java
@@ -7,20 +7,28 @@ import kg.peaksoft.peaksoftlmsm1.controller.dto.group.GroupResponse;
 import kg.peaksoft.peaksoftlmsm1.controller.dto.responseAll.GroupResponseAll;
 import kg.peaksoft.peaksoftlmsm1.db.service.GroupService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Slf4j
-@CrossOrigin(origins = "*", allowedHeaders = "*",maxAge = 3600)
+
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "Group controller", description = "ADMIN create, update and delete")
-@RequestMapping("api/admin/groups")
+@RequestMapping("api/groups")
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
+@Tag(name = "Group API", description = "Admin group endpoints")
 public class GroupController {
 
     private final GroupService groupService;
@@ -28,16 +36,14 @@ public class GroupController {
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method create", description = "admin can registration group")
     @PostMapping
-    public ResponseEntity<GroupResponse> create(@RequestBody @Valid GroupRequest request){
-        log.info("inside GroupController create method");
+    public ResponseEntity<GroupResponse> create(@RequestBody @Valid GroupRequest request) {
         return new ResponseEntity<>(groupService.create(request), HttpStatus.CREATED);
     }
 
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method update", description = "admin can update group")
     @PutMapping("{id}")
-    public ResponseEntity<GroupResponse> update(@PathVariable Long id, @Valid @RequestBody GroupRequest request){
-        log.info("inside GroupController update method");
+    public ResponseEntity<GroupResponse> update(@PathVariable Long id, @Valid @RequestBody GroupRequest request) {
         GroupResponse groupResponse = groupService.update(id, request);
         return new ResponseEntity<>(groupResponse, HttpStatus.OK);
     }
@@ -46,7 +52,6 @@ public class GroupController {
     @Operation(summary = "method get by id", description = "admin, instructor can get by id")
     @GetMapping("{id}")
     public ResponseEntity<GroupResponse> getById(@PathVariable Long id) {
-        log.info("inside GroupController get By Id method");
         return ResponseEntity.ok(groupService.getById(id));
     }
 
@@ -54,7 +59,6 @@ public class GroupController {
     @Operation(summary = "method delete", description = "admin can delete")
     @DeleteMapping("{id}")
     public ResponseEntity<GroupResponse> delete(@PathVariable Long id) {
-        log.info("inside GroupController delete method");
         groupService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -63,8 +67,7 @@ public class GroupController {
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @Operation(summary = "method get all", description = "admin can get all groups")
     public GroupResponseAll getAll(@RequestParam int size,
-                                   @RequestParam int page){
-        log.info("inside GroupController get all method");
+                                   @RequestParam int page) {
         return groupService.getAllGroups(size, page);
     }
 }
