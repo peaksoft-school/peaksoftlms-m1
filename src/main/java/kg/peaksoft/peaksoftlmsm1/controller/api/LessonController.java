cdf7d1f4de034de7ff7a446ad496527b4379a273
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 13:38:49 +0600

    refactor LessonController

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/LessonController.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/LessonController.java
@@ -6,15 +6,21 @@ import kg.peaksoft.peaksoftlmsm1.controller.dto.lesson.LessonRequest;
 import kg.peaksoft.peaksoftlmsm1.controller.dto.lesson.LessonResponse;
 import kg.peaksoft.peaksoftlmsm1.db.service.LessonService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Slf4j
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
 @RequiredArgsConstructor
@@ -27,16 +33,14 @@ public class LessonController {
     @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
     @Operation(summary = "method create", description = "Only Instructor can create lesson")
     @PostMapping
-    public ResponseEntity<LessonResponse> create(@RequestBody @Valid LessonRequest request){
-        log.info("inside LessonController create method");
+    public ResponseEntity<LessonResponse> create(@RequestBody @Valid LessonRequest request) {
         return new ResponseEntity<>(lessonService.create(request), HttpStatus.CREATED);
     }
 
     @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
     @Operation(summary = "method update", description = "Only Instructor can update lesson")
     @PutMapping("{id}")
-    public ResponseEntity<LessonResponse> update(@PathVariable Long id, @Valid @RequestBody LessonRequest request){
-        log.info("inside LessonController update method");
+    public ResponseEntity<LessonResponse> update(@PathVariable Long id, @Valid @RequestBody LessonRequest request) {
         LessonResponse lessonResponse = lessonService.update(id, request);
         return new ResponseEntity<>(lessonResponse, HttpStatus.OK);
     }
@@ -45,7 +49,6 @@ public class LessonController {
     @Operation(summary = "method get by id", description = "Instructor can get by id lesson")
     @GetMapping("{id}")
     public ResponseEntity<LessonResponse> getById(@PathVariable Long id) {
-        log.info("inside LessonController get By Id method");
         return ResponseEntity.ok(lessonService.getById(id));
     }
 
@@ -54,7 +57,6 @@ public class LessonController {
     @DeleteMapping("{id}")
     public ResponseEntity<LessonResponse> delete(@PathVariable Long id) {
         lessonService.delete(id);
-        log.info("inside LessonController delete method");
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
