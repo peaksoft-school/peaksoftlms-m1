f508aa51643eed3ac2800023461f3daedb7c76e1
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 17:02:02 +0600

    refactor PresentationResponse

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/dto/presentation/PresentationResponse.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/dto/presentation/PresentationResponse.java
@@ -1,7 +1,5 @@
 package kg.peaksoft.peaksoftlmsm1.controller.dto.presentation;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,12 +9,12 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class PresentationResponse {
 
     private Long id;
     private String name;
     private String description;
     private String file;
+
 }
 
