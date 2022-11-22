1b37ab88e8ecdf69e154c2f213c6552b92d10f1c
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 17:27:53 +0600

    refactor VideoLessonRequest

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/dto/videoLesson/VideoLessonRequest.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/dto/videoLesson/VideoLessonRequest.java
@@ -1,7 +1,5 @@
 package kg.peaksoft.peaksoftlmsm1.controller.dto.videoLesson;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,11 +9,10 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class VideoLessonRequest {
 
     private String name;
     private String description;
     private String link;
-//    private  Long lessonId;
+
 }
