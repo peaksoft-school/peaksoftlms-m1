a444dffab721db56c36bf43db5c961219d3e1903
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 16:09:26 +0600

    refactor PresentationController

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/PresentationController.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/PresentationController.java
@@ -6,15 +6,21 @@ import kg.peaksoft.peaksoftlmsm1.controller.dto.presentation.PresentationRequest
 import kg.peaksoft.peaksoftlmsm1.controller.dto.presentation.PresentationResponse;
 import kg.peaksoft.peaksoftlmsm1.db.service.PresentationService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Slf4j
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
 @RequiredArgsConstructor
@@ -27,16 +33,14 @@ public class PresentationController {
     @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
     @Operation(summary = "method create", description = "Only Instructor can create presentation")
     @PostMapping
-    public ResponseEntity<PresentationResponse> create(@RequestBody @Valid PresentationRequest request){
-        log.info("inside PresentationController create method");
+    public ResponseEntity<PresentationResponse> create(@RequestBody @Valid PresentationRequest request) {
         return new ResponseEntity<>(presentationService.create(request), HttpStatus.CREATED);
     }
 
     @PreAuthorize("hasAnyAuthority('ROLE_INSTRUCTOR')")
     @Operation(summary = "method update", description = "Only Instructor can update presentation")
     @PutMapping("{id}")
-    public ResponseEntity<PresentationResponse> update(@PathVariable Long id, @Valid @RequestBody PresentationRequest request){
-        log.info("inside PresentationController update method");
+    public ResponseEntity<PresentationResponse> update(@PathVariable Long id, @Valid @RequestBody PresentationRequest request) {
         PresentationResponse presentationResponse = presentationService.update(id, request);
         return new ResponseEntity<>(presentationResponse, HttpStatus.OK);
     }
@@ -45,7 +49,6 @@ public class PresentationController {
     @Operation(summary = "method get by id", description = "Instructor can get by id presentation")
     @GetMapping("{id}")
     public ResponseEntity<PresentationResponse> getById(@PathVariable Long id) {
-        log.info("inside PresentationController getById method");
         return ResponseEntity.ok(presentationService.getById(id));
     }
 
@@ -53,7 +56,6 @@ public class PresentationController {
     @Operation(summary = "method delete", description = "Instructor can delete presentation")
     @DeleteMapping("{id}")
     public ResponseEntity<PresentationResponse> delete(@PathVariable Long id) {
-        log.info("inside PresentationController delete method");
         presentationService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
