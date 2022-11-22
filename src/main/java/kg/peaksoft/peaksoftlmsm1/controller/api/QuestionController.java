19239e6dfae37b6c9d7ce92126892537493bc272
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-22 16:12:46 +0600

    refactor QuestionController

--- src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/QuestionController.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/controller/api/QuestionController.java
@@ -6,15 +6,21 @@ import kg.peaksoft.peaksoftlmsm1.db.service.testService.QuestionService;
 import kg.peaksoft.peaksoftlmsm1.controller.dto.test.request.QuestionRequest;
 import kg.peaksoft.peaksoftlmsm1.controller.dto.test.request.response.QuestionResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
@@ -29,7 +35,6 @@ public class QuestionController {
     @PostMapping("/{testId}")
     public ResponseEntity<QuestionResponse> save(@PathVariable Long testId,
                                                  @RequestBody QuestionRequest request) {
-        log.info("inside QuestionController create method");
         return new ResponseEntity<>(questionService.save(testId, request), HttpStatus.CREATED);
     }
 
@@ -38,7 +43,6 @@ public class QuestionController {
     @PutMapping("{id}")
     public ResponseEntity<QuestionResponse> update(@PathVariable Long id,
                                                    @RequestBody @Valid QuestionRequest request) {
-        log.info("inside QuestionController update method");
         QuestionResponse questionResponse = questionService.update(id, request);
         return new ResponseEntity<>(questionResponse, HttpStatus.OK);
     }
@@ -47,7 +51,6 @@ public class QuestionController {
     @Operation(summary = "method get by id", description = "Instructor can get by id Question")
     @GetMapping("{id}")
     public ResponseEntity<QuestionResponse> getById(@PathVariable Long id) {
-        log.info("inside QuestionController get by id method");
         return ResponseEntity.ok(questionService.getById(id));
     }
 
@@ -55,7 +58,6 @@ public class QuestionController {
     @Operation(summary = "method delete", description = "Instructor can delete Question")
     @DeleteMapping("{id}")
     public ResponseEntity<QuestionResponse> delete(@PathVariable Long id) {
-        log.info("inside QuestionController delete method");
         questionService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
