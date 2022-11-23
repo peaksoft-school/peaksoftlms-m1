a427ff5ecbcb3c4bc3906e45e3f0927a6e43612a
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-23 17:28:08 +0600

    refactor FileService

--- src/main/java/kg/peaksoft/peaksoftlmsm1/db/service/FileService.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/db/service/FileService.java
@@ -1,15 +1,17 @@
 package kg.peaksoft.peaksoftlmsm1.db.service;
 
 import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 public interface FileService {
 
-     String saveFile(MultipartFile file);
+    String saveFile(MultipartFile file);
 
-     byte[] downloadFile(Long id);
+    byte[] downloadFile(Long id);
 
-     String deleteFile(Long id);
+    String deleteFile(Long id);
 
-     List<String> listAllFiles();
+    List<String> listAllFiles();
+
 }
