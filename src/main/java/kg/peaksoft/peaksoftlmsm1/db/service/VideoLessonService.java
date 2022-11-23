677b2e6d865875bc78d43d3dfa550413c173101a
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-23 17:43:02 +0600

    refactor VideoLessonService

--- src/main/java/kg/peaksoft/peaksoftlmsm1/db/service/VideoLessonService.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/db/service/VideoLessonService.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Slf4j
@@ -22,24 +23,24 @@ public class VideoLessonService {
     private final VideoLessonEditMapper editMapper;
     private final VideoLessonViewMapper viewMapper;
 
-    public VideoLessonResponse create(VideoLessonRequest request){
+    public VideoLessonResponse create(VideoLessonRequest request) {
         VideoLesson videoLesson = editMapper.create(request);
         repository.save(videoLesson);
         log.info("Entity videoLesson save: {}", videoLesson.getName());
         return viewMapper.mapperResponse(videoLesson);
     }
 
-    public VideoLessonResponse update(Long id,VideoLessonRequest request){
+    public VideoLessonResponse update(Long id, VideoLessonRequest request) {
         Optional<VideoLesson> videoLesson = Optional.ofNullable(repository.findById(id).orElseThrow(() -> {
             log.error("Entity videoLesson with id = {} does not exists in database", id);
             throw new ResourceNotFoundException("Entity", "id", id);
         }));
-        editMapper.update(videoLesson.get(),request);
+        editMapper.update(videoLesson.orElseThrow(NoSuchElementException::new), request);
         log.info("Entity videoLesson updated: {}", id);
         return viewMapper.mapperResponse(repository.save(videoLesson.get()));
     }
 
-    public VideoLessonResponse getById(Long id){
+    public VideoLessonResponse getById(Long id) {
         log.info("Get entity videoLesson by id: {}", id);
         return viewMapper.mapperResponse(repository.findById(id).orElseThrow(() -> {
             log.error("Entity videoLesson with id = {} does not exists in database", id);
