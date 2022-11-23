79c677dc5304810694ad9f3bb0eded436fb0e1ad
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-23 15:04:37 +0600

    refactor StorageConfig

--- src/main/java/kg/peaksoft/peaksoftlmsm1/config/StorageConfig.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/config/StorageConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AmazonConfig {
+public class StorageConfig {
 
     @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
