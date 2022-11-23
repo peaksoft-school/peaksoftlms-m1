726b8ecd5156642b6ff2780cd0b8579045ed5c2a
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-23 17:12:56 +0600

    refactor VideoLesson entity

--- src/main/java/kg/peaksoft/peaksoftlmsm1/db/entity/VideoLesson.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/db/entity/VideoLesson.java
@@ -1,28 +1,36 @@
 package kg.peaksoft.peaksoftlmsm1.db.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import kg.peaksoft.peaksoftlmsm1.db.entity.Lesson;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
-@Entity
-@Table(name = "videoLesson")
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "video_lessons")
 public class VideoLesson {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "videoLesson_gen")
-    @SequenceGenerator(name = "videoLesson_gen",sequenceName = "videoLesson_seq",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "video_lesson_gen")
+    @SequenceGenerator(name = "video_lesson_gen", sequenceName = "video_lesson_seq", allocationSize = 1)
     private Long id;
+
     private String name;
+
     private String description;
+
     private String link;
 
     @JsonIgnore
--- src/main/resources/db/migration/V1__create_DB.sql
+++ src/main/resources/db/migration/V1__create_DB.sql
@@ -99,7 +99,7 @@ create table users_groups (user_id int8 not null,
 create table users_roles (user_id int8 not null,
                           roles_id int8 not null);
 
-create table video_lesson (id int8 not null,
+create table video_lessons (id int8 not null,
                            description varchar(255),
                            link varchar(255),
                            name varchar(255),
