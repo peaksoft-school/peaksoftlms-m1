6faee02c7e3243020258df407dc3499e64b46b39
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-23 15:02:30 +0600

    refactor SpringSecurity

--- src/main/java/kg/peaksoft/peaksoftlmsm1/config/security/SpringSecurity.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/config/security/SpringSecurity.java
@@ -1,7 +1,7 @@
 package kg.peaksoft.peaksoftlmsm1.config.security;
 
 import kg.peaksoft.peaksoftlmsm1.db.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,33 +13,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SpringSecurity extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
     private final JwtTokenFilter jwtTokenFilter;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    @Autowired
-    BCryptPasswordEncoder passwordEncoder;
-
-    public SpringSecurity(UserService userService, JwtTokenFilter jwtTokenFilter) {
-        this.userService = userService;
-        this.jwtTokenFilter = jwtTokenFilter;
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
-
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http.cors().and().csrf().disable()
-
                 .authorizeRequests()
                 .antMatchers("/api/public/**").permitAll()
-                .antMatchers( "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
