19f8a672975a3cdd0c596d92f79e6a7929583ada
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-23 16:18:46 +0600

    refactor Course entity

--- src/main/java/kg/peaksoft/peaksoftlmsm1/db/entity/Course.java
+++ src/main/java/kg/peaksoft/peaksoftlmsm1/db/entity/Course.java
@@ -3,11 +3,25 @@ package kg.peaksoft.peaksoftlmsm1.db.entity;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -25,12 +39,16 @@ public class Course {
     @SequenceGenerator(name = "course_gen", sequenceName = "course_seq", allocationSize = 1)
     @Column(name = "id")
     private Long id;
+
     @Column(name = "course_img")
     private String image;
+
     @Column(name = "name_course")
     private String nameCourse;
+
     @Column(name = "start_course")
     private Date startCourse;
+
     @Column(name = "description")
     private String description;
 
@@ -49,7 +67,7 @@ public class Course {
     private List<Group> groups;
 
     @JsonIgnore
-    @OneToMany(mappedBy ="course" ,fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
     private List<Lesson> lessons;
 
     public void setUsers(User user) {
@@ -61,7 +79,6 @@ public class Course {
 
     /**
      * эта аннотация убирает конфликт двух сеттеров
-     * @param users
      */
     @JsonBackReference("users")
     public void setUsers(List<User> users) {
